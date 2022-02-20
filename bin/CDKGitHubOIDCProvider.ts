54c9a45cf3d7772e1f0070b623243a463017dc31
Shahab Dogar <shahab96@gmail.com>
2022-02-19 20:30:01 -0800

    Initial commit

---
+++ bin/CDKGitHubOIDCProvider.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import {
+  aws_iam as iam,
+} from 'aws-cdk-lib';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'CdkGitHubOIDCProvider');
+
+const provider = new iam.OpenIdConnectProvider(stack, 'GitHubOIDCProvider', {
+  url: 'https://token.actions.githubusercontent.com',
+  clientIds: ['sts.amazonaws.com'],
+});
+
+stack.exportValue(provider.openIdConnectProviderArn, {
+  name: 'GitHubOIDCProviderArn',
+});
